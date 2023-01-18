2414162e9404027f32f6a5d7a90cf3550c765bad
Egor Machnev <egorikhelp@gmail.com>
2023-01-19 00:52:31 +0300

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,1 @@
+export * from "https://deno.land/x/suppress@1.0.0/mod.ts";
---
+++ mod.ts
@@ -0,0 +1,55 @@
+import { ResultType, suppress } from "./deps.ts";
+
+import type { Result } from "./deps.ts";
+
+export type Fetch<Key, Model> = (key: Key) => Promise<Model>;
+
+export interface CacheMap<Key, Model> {
+  get(key: Key): Result<Model> | undefined;
+  set(key: Key, value: Result<Model>): void;
+  delete(key: Key): void;
+}
+
+export class DataCache<Key, Model> {
+  private readonly fetch: Fetch<Key, Model>;
+  private readonly cache: CacheMap<Key, Model>;
+
+  constructor(fetch: Fetch<Key, Model>, cache: CacheMap<Key, Model>) {
+    this.fetch = fetch;
+    this.cache = cache;
+  }
+
+  async load(key: Key): Promise<Model> {
+    const cached = this.cache.get(key);
+    if (cached) {
+      return this.resolve(cached);
+    }
+
+    const result = await suppress<Model>(
+      () => this.fetch(key),
+    );
+
+    this.cache.set(key, result);
+    return this.resolve(result);
+  }
+
+  update(key: Key, value: Model): void {
+    this.cache.set(
+      key,
+      { type: ResultType.SUCCESS, data: value },
+    );
+  }
+
+  delete(key: Key): void {
+    this.cache.delete(key);
+  }
+
+  private resolve(result: Result<Model>): Model {
+    switch (result.type) {
+      case ResultType.SUCCESS:
+        return result.data;
+      case ResultType.FAILURE:
+        throw new Error(result.message);
+    }
+  }
+}
