e2749deac19343fdbb57c3901c5d64699b18bb4d
Egor Machnev <egorikhelp@gmail.com>
2023-01-19 16:55:30 +0300

    Adds the TTLMap from the manual example.

--- ttl.ts
+++ ttl.ts
@@ -1,55 +1,36 @@
-import { ResultType, suppress } from "./deps.ts";
+import type { Cache } from "./mod.ts";
 
-import type { Result } from "./deps.ts";
-
-export type { Result };
-
-export type Fetch<Key, Model> = (key: Key) => Model | Promise<Model>;
-
-export interface Cache<Key, Value> {
-  get(key: Key): Value | undefined;
-  set(key: Key, value: Value): void;
-  delete(key: Key): void;
+export interface Cached<Value> {
+  value: Value;
+  expires: number;
 }
 
-export class DataCache<Key, Model> {
-  private readonly fetch: Fetch<Key, Model>;
-  private readonly cache: Cache<Key, Result<Model>>;
+export class TTLMap<Key, Value> implements Cache<Key, Value> {
+  private readonly map: Map<Key, Cached<Value>>;
+  private readonly ttl: number;
 
-  constructor(fetch: Fetch<Key, Model>, cache: Cache<Key, Result<Model>>) {
-    this.fetch = fetch;
-    this.cache = cache;
+  constructor(ms: number) {
+    this.map = new Map();
+    this.ttl = ms;
   }
 
-  async load(key: Key): Promise<Model> {
-    const cached = this.cache.get(key);
-    if (cached) {
-      return this.resolve(cached);
+  get(key: Key): Value | undefined {
+    const item = this.map.get(key);
+    if (item) {
+      if (item.expires > Date.now()) {
+        return item.value;
+      }
+      this.map.delete(key);
     }
-
-    const result = await suppress(() => this.fetch(key));
-
-    this.cache.set(key, result);
-    return this.resolve(result);
+    return undefined;
   }
 
-  update(key: Key, value: Model): void {
-    this.cache.set(
-      key,
-      { type: ResultType.SUCCESS, data: value },
-    );
+  set(key: Key, value: Value): void {
+    const expires = Date.now() + this.ttl;
+    this.map.set(key, { value, expires });
   }
 
   delete(key: Key): void {
-    this.cache.delete(key);
-  }
-
-  private resolve(result: Result<Model>): Model {
-    switch (result.type) {
-      case ResultType.SUCCESS:
-        return result.data;
-      case ResultType.FAILURE:
-        throw new Error(result.message);
-    }
+    this.map.delete(key);
   }
 }
